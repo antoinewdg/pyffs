97dea6ac80493df33c7435090c8aa29c987ef9d6
antoinewdg <antoinewendlinger@gmail.com>
2016-09-23 22:04:35 +0200

    Move test module

--- test/transition_matrix/test_manager.py
+++ test/transition_matrix/test_manager.py
@@ -2,9 +2,9 @@ from time import time
 
 import pytest
 
-from pyffs.transition_matrix.manager import Manager
 from pyffs.automaton_generation import generate_transition_matrix_and_save_to_file
-from pyffs.test.utils import clean_generated_dir
+from pyffs.transition_matrix.manager import Manager
+from test.utils import clean_generated_dir
 
 
 class TestManager:
