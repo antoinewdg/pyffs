1f668950e488895732d4b89fcabe7778db6c53f7
antoinewdg <antoinewendlinger@gmail.com>
2016-09-24 17:31:30 +0200

    Fix the generation command

--- pyffs/bin/pyffs_generate.py
+++ pyffs/bin/pyffs_generate.py
@@ -3,19 +3,13 @@ from os.path import dirname, join
 
 import click
 
-from pyffs.automaton_generation import generate_transition_matrix
-from pyffs.transition_matrix import save_matrix_to_file
-from pyffs.settings import GENERATED_DIR, MATRIX_FILE_NAMING
+from pyffs.automaton_management import generate_automaton_to_file
 
 
 @click.command()
 @click.argument('tolerance', type=int)
 def main(tolerance):
-    matrix, bit_vectors, n_states = generate_transition_matrix(tolerance)
-
-    filename = join(GENERATED_DIR, MATRIX_FILE_NAMING % tolerance)
-    with open(filename, 'w+') as file:
-        save_matrix_to_file(matrix, file, bit_vectors, n_states)
+    generate_automaton_to_file(tolerance)
 
 
 if __name__ == "__main__":
