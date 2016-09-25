b5f8eaf0876dc5ed6996d8c7e70d682c169a33cb
antoinewdg <antoinewendlinger@gmail.com>
2016-09-25 21:24:03 +0200

    Improve performance
    Change dict access to a list access by associating a bit vector to an id.

--- pyffs/core/universal_automaton.py
+++ pyffs/core/universal_automaton.py
@@ -28,8 +28,8 @@ def write_to_file(automaton: UniversalAutomaton, file):
 
     for i in range(len(automaton.states)):
         row = []
-        for bv in automaton.bit_vectors:
-            s = automaton.matrix[i][bv]
+        for bv_id, bv in enumerate(automaton.bit_vectors):
+            s = automaton.matrix[i][bv_id]
             row.append("%s,%s" % (s.id, s.min_boundary))
         writer.writerow(row)
 
@@ -62,11 +62,11 @@ def read_from_file(file):
 
     bit_vectors = [_bit_vector_for_string(s) for s in next(reader)]
     max_i_minus_e = [int(v) for v in next(reader)]
-    matrix = [{} for _ in range(len(states))]
+    matrix = [[] for _ in range(len(states))]
 
     for id_, line in enumerate(reader):
         for b_index, s in enumerate(line):
-            matrix[id_][bit_vectors[b_index]] = state_from_string(s)
+            matrix[id_].append(state_from_string(s))
 
     return UniversalAutomaton(states, bit_vectors, matrix, max_i_minus_e)
 
