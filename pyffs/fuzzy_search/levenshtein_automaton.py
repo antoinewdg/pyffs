530eda36af2e09e044b944b4532736e7f4bc33c5
antoinewdg <antoinewendlinger@gmail.com>
2016-09-27 12:24:19 +0200

    Try using tox

--- pyffs/fuzzy_search/levenshtein_automaton.py
+++ pyffs/fuzzy_search/levenshtein_automaton.py
@@ -24,21 +24,21 @@ class LevenshteinAutomaton:
             bit_list.append(int(c == symbol))
         return self._bit_vectors_ids[tuple(bit_list)]
 
-    def transition(self, state: State, symbol):
+    def transition(self, state, symbol):
         bit_vector = self._precomputed_bit_vectors[state.min_boundary][symbol]
         new_state = self._automaton.matrix[state.id][bit_vector]
         return State(new_state.id, new_state.min_boundary + state.min_boundary)
 
-    def is_final(self, state: State):
+    def is_final(self, state):
         i_minus_e = self._automaton.max_i_minus_e[state.id]
         return i_minus_e + state.min_boundary >= self._i_minus_e_threshold
 
-    def get_error(self, state: State):
+    def get_error(self, state):
         i_minus_e = self._automaton.max_i_minus_e[state.id]
         return len(self._word) - (i_minus_e + state.min_boundary)
 
     @staticmethod
-    def is_empty_state(state: State):
+    def is_empty_state(state):
         return state.id == -1
 
     @staticmethod
