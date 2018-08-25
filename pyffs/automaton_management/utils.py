1f97630610260707dedee5db8b65f336756f9eeb
Roman Inflianskas <infroma@gmail.com>
2018-08-25 10:11:54 +0300

    generate_automaton_to_file: add overwrite_existing argument

--- pyffs/automaton_management/utils.py
+++ pyffs/automaton_management/utils.py
@@ -1,15 +1,15 @@
 from pathlib import Path
-from os.path import join
 
 from pyffs.automaton_generation import generate_universal_automaton
 from pyffs.settings import GENERATED_DIR, MATRIX_FILE_NAMING
 from pyffs.core.universal_automaton import write_to_file
 
 
-def generate_automaton_to_file(tolerance):
-    automaton = generate_universal_automaton(tolerance)
-
-    Path(GENERATED_DIR).mkdir(parents=True, exist_ok=True)
-    filename = join(GENERATED_DIR, MATRIX_FILE_NAMING % tolerance)
-    with open(filename, 'w+') as file:
-        write_to_file(automaton, file)
+def generate_automaton_to_file(tolerance, overwrite_existing=False):
+    generated_dir = Path(GENERATED_DIR)
+    filename = generated_dir / (MATRIX_FILE_NAMING % tolerance)
+    if not filename.exists() or overwrite_existing:
+        generated_dir.mkdir(parents=True, exist_ok=True)
+        with open(str(filename), 'w+') as file:
+            automaton = generate_universal_automaton(tolerance)
+            write_to_file(automaton, file)
